fc5a5052f2d611afac0562aabefa1d1d098e65d1
unknown <ianmmah@gmail.com>
2021-03-05 23:13:30 -0800

    Simplify reptile contract

--- src/contracts/Reptile.sol
+++ src/contracts/Reptile.sol
@@ -3,8 +3,7 @@ pragma solidity 0.5.0;
 import "./ERC721Full.sol";
 
 contract Reptile is ERC721Full {
-  ReptileStruc[] public reptiles;
-  mapping(uint256 => ReptileStruc) public tokenProperty;
+  mapping(uint256 => ReptileStruc) public reptiles;
 
   uint nonce = 0;
 
@@ -18,14 +17,12 @@ contract Reptile is ERC721Full {
 
   function mint(string memory _species, string memory _name) public {
     uint _tokenId = totalSupply();
-
-    items.push(PenguinStruc({id: _tokenId, species: _species, name: _name}));
-    tokenProperty[_tokenId] = PenguinStruc({id: _tokenId, species: _species, name: _name});
+    reptiles[_tokenId] = ReptileStruc({id: _tokenId, species: _species, name: _name});
     _mint(msg.sender, _tokenId);
   }
 
-  function getTokenProperties(uint256 _tokenId) external view returns (string memory _id, string memory _species, uint256 _name) {
-    return (tokenProperty[_tokenId].id, tokenProperty[_tokenId].asd, tokenProperty[_tokenId].attack);
+  function getTokenProperties(uint256 _tokenId) external view returns (uint256 _id, string memory _species, string memory _name) {
+    return (reptiles[_tokenId].id, reptiles[_tokenId].species, reptiles[_tokenId].name);
   }
 
 }
