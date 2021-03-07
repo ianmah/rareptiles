d6c1e40e4f22b5a51102cf2c925d4e67df8e2cf5
unknown <ianmmah@gmail.com>
2021-03-06 16:32:40 -0800

    fonts

--- src/components/Button.js
+++ src/components/Button.js
@@ -13,6 +13,9 @@ const StyledButton = styled.button`
   flex-basis: calc(50% - 20px);
   transition: all 200ms cubic-bezier(0.25, 0.8, 0.25, 1);
   box-shadow: 3px 3px 8px #e3e2e1;
+  font-family: 'Fira Mono', monospace;
+  text-transform: uppercase;
+  font-size: 12px;
 
   &:active {
     transform: translate(-2px, 2px);
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,4 +2,6 @@ import styled from 'styled-components'
 
 export const Title = styled.h3`
     margin: 0;
+    font-family: 'Shapiro';
+    text-transform: uppercase;
 `
