a2fdb42cbf02b5a1f43393d66993f4fba4c01792
unknown <ianmmah@gmail.com>
2021-03-06 19:02:56 -0800

    Add logo

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
+import logo from '../assets/reptile.png';
 
 const StyledSidebar = styled.div`
-    min-width: 220px;
+    width: 240px;
     float: left;
     height: 100vh;
     background: #fff;
@@ -24,11 +25,31 @@ const MenuItem = styled.a`
     ${p => p.active ? 'border-left: #de4e4e 6px solid;' : 'border-left: #de4e4e 0px solid;'}
 `
 
+const Logo = styled.img`
+    width: 36px;
+`
+
+const SidebarHeader = styled.h1`
+    margin: 0;
+    font-size: 24px;
+    display: inline;
+    margin-left: 10px;
+`
+
+const Header = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 1.5em;
+`
+
 const Sidebar = ({ activePage, setActivePage }) => {
 
     return (
         <StyledSidebar>
-            <h1>Rareptiles</h1>
+            <Header>
+                <Logo src={logo} alt="logo" />
+                <SidebarHeader>Rareptiles</SidebarHeader>
+            </Header>
             <MenuItem active={activePage === 'marketplace'} onClick={() => setActivePage('marketplace')}>Marketplace</MenuItem>
             <MenuItem active={activePage === 'collection'} onClick={() => setActivePage('collection')}>Collection</MenuItem>
             <MenuItem active={activePage === 'shelter'} onClick={() => setActivePage('shelter')}>Shelter</MenuItem>
